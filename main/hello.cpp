6d741509546ef4914bdf38d63f429c0f984541b1
Gowtham S <gowthams090903@gmail.com>
2024-03-06 00:50:31 +0530

    Webhook created and enabled

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "A change to hello.cpp";
-  cout << "Webhook enabled" ;
+  cout << "A change to hello.cpp\n";
+  cout << "Webhook enabled\n" ;
   return 0;
 }
